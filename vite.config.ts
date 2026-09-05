d16b4575943213b24b66e67a5ca8ad077a1b99bf
quanDA02 <quanda2k3@gmail.com>
2026-09-05 18:28:57 +0700

    config

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/react-weather-application",
+  base: "/react-weather-application/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
